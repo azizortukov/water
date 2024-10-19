3baf88b55be3ac4cc883b9fa6e630feb61de272c
rahmatillojuraev <raxmatillojurayev94@gmail.com>
2024-10-19 16:20:49 +0500

    Created Calculator class

---
+++ src/main/java/uz/aziz/water/util/Calculator.java
@@ -0,0 +1,10 @@
+package uz.aziz.water.util;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        System.out.println("Adding " + a + " and " + b);
+        return a + b;
+    }
+
+}
